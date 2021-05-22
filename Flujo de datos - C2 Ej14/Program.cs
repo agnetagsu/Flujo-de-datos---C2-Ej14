482873156443962540adfda01942eeb6d2d3ac10
Agneta <agnetagsu@yahoo.es>
2021-05-22 20:59:18 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Flujo_de_datos___C2_Ej14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double radio;
+            double area;
+
+            Console.WriteLine("Introduce el radio:");
+            radio = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI * radio * radio;
+            Console.WriteLine("El área es "+ area);
+        }
+    }
+}
